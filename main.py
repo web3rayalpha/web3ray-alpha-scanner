916a19b918b4d12ecd70bda2831369a536a90a88
web3rayalpha <web3ray1982@gmail.com>
2026-07-26 13:31:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import os
 import time
-from scanner_v4 import get_new_tokens
+from scanner_v5 import get_new_tokens
 
 
 def main():
     token = os.getenv("BOT_TOKEN")
     chat_id = os.getenv("CHAT_ID")
-    api_key = os.getenv("BIRDEYE_API_KEY")
 
-    if not token or not chat_id or not api_key:
-        print("Missing BOT_TOKEN, CHAT_ID or BIRDEYE_API_KEY")
+    if not token or not chat_id:
+        print("Missing BOT_TOKEN or CHAT_ID")
         return
 
-    print("WEB3RAY V4 STARTED")
+    print("WEB3RAY V5 STARTED")
 
     while True:
-        get_new_tokens(token, chat_id, api_key)
+        get_new_tokens(token, chat_id)
         time.sleep(30)
 
 
