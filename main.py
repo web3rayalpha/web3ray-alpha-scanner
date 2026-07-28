52bed9a7de00e44e7cc3392b72162070e1fc6919
web3rayalpha <web3ray1982@gmail.com>
2026-07-28 11:50:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import os
 import time
-from scanner_v5 import get_new_tokens
+from scanner_v6 import get_new_pools
 
 
 def main():
     token = os.getenv("BOT_TOKEN")
     chat_id = os.getenv("CHAT_ID")
+    api_key = os.getenv("COINGECKO_API_KEY")
 
-    if not token or not chat_id:
-        print("Missing BOT_TOKEN or CHAT_ID")
-        return
+    if not token:
+        raise Exception("BOT_TOKEN missing")
 
-    print("WEB3RAY V5 STARTED")
+    if not chat_id:
+        raise Exception("CHAT_ID missing")
+
+    if not api_key:
+        raise Exception("COINGECKO_API_KEY missing")
+
+    print("WEB3RAY V6 STARTED")
 
     while True:
-        get_new_tokens(token, chat_id)
-        time.sleep(30)
+        try:
+            get_new_pools(
+                token=token,
+                chat_id=chat_id,
+                api_key=api_key
+            )
+        except Exception as e:
+            print("ERROR:", e)
+
+        time.sleep(60)
 
 
 if __name__ == "__main__":
